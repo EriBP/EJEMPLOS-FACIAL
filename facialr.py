67b81e0ba00a8ea510069ebc631414e91ae5b28a
EriBP <ebarojas@uv.mx>
2022-06-18 22:48:57 -0500

    corrio con `exito

--- facialr.py
+++ facialr.py
@@ -1,5 +1,5 @@
 from deepface import DeepFace
 
-obj = DeepFace.analyze(img_path = "erika.jpg", actions = ['age', 'gender', 'race', 'emotion'])
+obj = DeepFace.analyze(img_path = "prueba.jpg", actions = ['age', 'gender', 'race', 'emotion'])
 
 print (obj)
